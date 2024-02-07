436c6e873a6fface714c6f0d258de15d0ddad6c7
Sylvain <sylviemontoute@gmail.com>
2024-02-07 14:09:50 -0400

    Design Home page of Participant role

--- participant.php
+++ participant.php
@@ -2,6 +2,30 @@
 
  ?>
 <body>
-<p>The information goes here</p>
+<header>
+        <h1>Welcome</h1>
+        <p id="announcementsPlaceholder">Announcements Placeholder</p>
+    </header>
+
+    <section>
+        <h2>Pregnancy Registration and Data Collection</h2>
+        <p>
+        This is where you can place any announcements.
+        </p>
+    </section>
+
+    <section>
+        <h2>Two Containers Side by Side</h2>
+        <div class="container-wrapper">
+            <div class="container">
+                <h3>Your Schedule</h3>
+                <p>This is the content of your schedule</p>
+            </div>
+            <div class="container">
+                <h3>Your Resources</h3>
+                <p>This is the content of your resources</p>
+            </div>
+        </div>
+    </section>
 </body>
 </html>
