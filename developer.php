62994f6bbfd83622f6b389237078244e3d8c0aea
adieddygit <adiemmanuel10@gmail.com>
2024-02-08 18:31:39 +0000

    developer commit

--- developer.php
+++ developer.php
@@ -2,6 +2,45 @@
 
  ?>
 <body>
-<p></p>
+<section class="row"> 
+<div class="col-s-3">
+<div class="dev_sidenav">
+<h5>CORE</h5>
+<a href="#">Home</a>
+<h5>DOWNLOADS</h5>
+<a href="#">Manuals</a>
+<div class="dev_dropdown">
+<h5>FUNCTIONALITIES</h5>
+<a href="#">Pages</a>
+<div class="dev_dropdown-content">
+<a href="#">Users & Accounts</a>
+<a href="#">Create Training</a>
+</div>
+
+<footer>
+Loged in as:<br><br>
+Test Administrator
+</footer> 
+</div>
+</div>
+</div>
+
+
+<article class="col-s-9">
+    <table>
+      <tr>
+ <th class="head"><h2>Training & Certification System</h2></th>
+  </tr>
+  <tr>
+      <th  class="body">
+      <strong>Welcome Test Administrator</strong>
+      <p>Welcome Test Administrator</p>
+      <p>Welcome Test Administrator</p>
+      <p>Welcome Test Administrator</p>
+      </th>
+  </tr>
+    </table>
+</article>
+</section>
 </body>
 </html>
