d28603bc436a8cf7da5c5abac17ce82ee40896c7
unknown <karonstical@gmail.com>
2024-02-08 10:57:24 -0800

    keep going

--- surveillance.php
+++ surveillance.php
@@ -2,6 +2,38 @@
   require_once "header.php";
  ?>
 <body>
+  <p>The information goes here</p>
+  <p>About to get started</p>
+<section class="S_grid_container">
+  <div class="row">
+       <div class="col-xl-6 col-md-6">
+           <div class="card bg-success mb-4">
+             <h2 class='card-header bg-success'>Patient Management & Reporting System</h2>
+             <h3 >Reporting</h3>
+               <div class="card-body bg-white">
+                 <h5>Welcome test surveillance</h5>
+                  <p>The countries of the Organisation of Eastern Caribbean States (OECS) have strategically invested in strengthening case-based surveillance
+                  of HIV/STI, TB and Viral Hepatitis in an attempt to improve the availability of good quality data in order to monitor the evolution of the epidemic
+                and the effect of public health and clinical interventions in a sustainable manner. In addition, establishing the framework for
+              case-based surveillance for HIV will ultimately allow for the use of this approach for other health conditions, especially the non-communicable diseases (NCDs).
+              The result therefore is that CBS will result in significant strengthening of the information systems in the OECS allowing for better, evidence-informed decision at both the clinical and administrative levels.
+            This advance integrated HIV/STI/TB case-based surveillance system collects the following data:<br/><br/>
+
+                <i> 1. HIV/STI, TB and Viral Hepatitis Screening Information for Pregnant, Adult, Pediatric and Exposed Infants<br/><br/>
+                2. Behavioral data of registered / screened clients<br/><br/>
+                3. Prevention and Control data<br/><br/>
+                4. ANC & PMTCT data (HIV, Syphilis and Viral Hepatitis)<br/><br/>
+                5. Clinical Management of HIV/STI, TB and Viral Hepatitis<br/><br/>
+                6. Central Medical Supplies and Pharmacy Stock and MEdical Dispense<br/><br/>
+                7. Auto-reporting & data story telling using defined reporting indicators on the data collected<br/><br/></i>
+                </p>
+                <div class="card-footer d-flex align-items-center justify-content-between">
+               </div>
+           </div>
+       </div>
+     </div>
+
+</section>
 <p>The information goes here</p>
 <p>About to get started</p>
 </body>
