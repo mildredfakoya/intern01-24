d55545c54485e311696ec8d78a8b78557e2d41bb
unknown <karonstical@gmail.com>
2024-02-10 14:35:30 -0800

    recreating th branch

--- surveillance.php
+++ surveillance.php
@@ -1,8 +1,82 @@
 <?php
   require_once "header.php";
  ?>
-<body>
-<p>The information goes here</p>
-<p>About to get started</p>
+<body class="S_home">
+  
+  <header>
+      <h2>Patient Managment & Reporting System</h2>
+      <h4>Reporting</h4>
+  </header>
+  
+  <section class="S_grid_container">
+      <nav class="S_sidenavbar">
+          <ul>
+              <li><a href="#">Home</a></li>
+          </br>
+              <li><a href="#">About</a></li>
+          </br>
+              <div class="S_dropdown">
+                  <button class="S_dropbtn">Dropdown</button>
+                  <div class="S_dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                  </div>
+              </div>
+          </br>
+          </br>
+          <li><a href="#">Home</a></li>
+      </br>
+          <li><a href="#">About</a></li>
+      </br>
+      </br>
+              
+              <div class="S_dropdown">
+                  <button class="S_dropbtn">Dropdown</button>
+                  <div class="S_dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                  </div>
+                  </div>
+              </br>
+              </br>
+                  <li><a href="#">Home</a></li>
+              </br>
+                  <li><a href="#">About</a></li>
+              </br>
+          
+              
+          </ul>
+
+      </nav>
+
+      <article class="S_article1">
+                             <h5>Welcome test surveillance</h5>
+               <p>Your assigned role is Monitoring and Reporting:</p>
+
+              <p>the pages menu on the left to reveal the set of functionalities you have under your assigned role.</p> 
+
+              <p> To get the Screening, Prevention/Control and OECS M&E auto-generated Report, expand the Screening,Prevention and Control sub-menu of the pages menu located at the side bar on the left of the screen.
+
+                  <p> 2. To get report on HIV, TB, Syphilis Cases OECS Summary Sheet, expand the Commitment to the 90-90-90 Target sub-menu.
+
+               3. To get reports on Antenatal and Exposed Infants, expand the Antenatal Reporting sub-menu.
+
+               4. To get pharmacy reports, expand the Pharmacy Report sub-menu.
+              </p> 
+      </article>
+
+      <aside class="S_aside1">
+          <h5>More Info</h5>
+      </aside>
+
+  </section>
+
+
+
+
+
+
 </body>
 </html>
