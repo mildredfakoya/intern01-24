0c189e7872e17ccf5c51667e11a206407b88e134
lawrence <lawoj91@hotmail.com>
2024-02-12 14:02:13 -0400

    recent updated administrator.php/css

--- administrator.php
+++ administrator.php
@@ -5,8 +5,8 @@ require_once "header.php";
 <body>
     <div class="a_wrapper">
         <nav>
-            <a class="nav-link" href="participant.php">Next</a>
-            <a class="nav-link" href="developer.php">Pervious</a>
+            <a class="nav-link" href="#">Ref NO</a>
+            <a class="nav-link" href="#">210775</a>
         </nav>
         <div class="a_administrator">
     <h1>PATIENT MANAGEMENT AND REPORTING SYSTEM ADMINISTRATION</h1>
@@ -27,7 +27,33 @@ require_once "header.php";
             <div class="form-field">
                 <label for="email">Email</label>
                 <input type="email" id="email" required>
-            </div>
+
+           <div class="form-field">
+        <label for="date-of-birth">Date of Birth</label>
+        <input type="date" id="date-of-birth" required>
+
+        <div class="form-field">
+        <label for="address">Address</label>
+        <input type="text" id="address" required>
+      </div>
+      <div class="form-field">
+        <label for="city">City</label>
+        <input type="text" id="city" required>
+      </div>
+      <div class="form-field">
+        <label for="state">State</label>
+        <input type="text" id="state" required>
+      </div>
+      <div class="form-field">
+        <label for="zip">Zip</label>
+        <input type="text" id="zip" required>
+        </div>
+        <div class="form-field">
+          <label for="phone-number">Phone Number</label>
+          <input type="text" id="phone-number" required>
+        </div>
+    
+          </div>
             <div class="form-field">
                 <button type="submit">Submit</button>
             </div>
